e93f6aeec3e8a224c9e6a2df2635f665725be8ff
Jack Waugh <m2hh2kmhsn@snkmail.com>
2014-08-29 01:20:27 -0400

    Clean out some arrogant cruft.  References related to programming-language project are in a file that is no longer referenced from the home page; might want to publish it again later or part of it.

--- src/left/2013-04-23_work.rb
+++ src/left/2013-04-23_work.rb
@@ -1,14 +1,13 @@
 div :class => "article_summary" do
   h2 "Compensated Work"
-  p "[2013-04-23]"
+  p "[2014-08-29 UCT]"
   p do
     text "I am not currently available to be recruited."
   end
   p do
     text "I associate with the"
-    text "Center for Communitarian Technological Services ("
     a "CCTS", :href => "http://cctsbaltimore.org/"
-    text ")."
+    text "."
   end
 end
 
